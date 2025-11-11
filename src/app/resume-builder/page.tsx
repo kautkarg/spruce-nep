b0ce2e35a97cf87e3e8a34866d84296dc73ec45d
Gaurav Kautkar <kautkargaurav@gmail.com>
2025-11-11 18:41:27 +0000

    Try fixing this error: `Runtime Error: Error: An unexpected response was

--- src/app/resume-builder/page.tsx
+++ src/app/resume-builder/page.tsx
@@ -1,7 +1,29 @@
 
+"use client";
+
 import { AppFooter } from "@/components/common/Footer";
 import { Header } from "@/components/common/Header";
-import { ResumeBuilder } from "@/components/resume-builder/ResumeBuilder";
+import { Skeleton } from "@/components/ui/skeleton";
+import dynamic from "next/dynamic";
+
+const ResumeBuilder = dynamic(() => import('@/components/resume-builder/ResumeBuilder').then(mod => mod.ResumeBuilder), {
+    ssr: false,
+    loading: () => (
+        <div className="container mx-auto py-8 md:py-12 px-4">
+            <div className="grid grid-cols-1 lg:grid-cols-12 gap-8">
+                <div className="lg:col-span-5 xl:col-span-4 space-y-6">
+                    <Skeleton className="h-10 w-full" />
+                    <Skeleton className="h-20 w-full" />
+                    <Skeleton className="h-40 w-full" />
+                    <Skeleton className="h-40 w-full" />
+                </div>
+                <div className="lg:col-span-7 xl:col-span-8">
+                    <Skeleton className="w-full aspect-[8.5/11]" />
+                </div>
+            </div>
+        </div>
+    ),
+});
 
 export default function ResumeBuilderPage() {
     return (
